4d70e0903e38e593ff20de4ee36990fd0c6992a3
wangli <charlesw1234@163.com>
2016-02-21 18:56:38 +0800

    * show more.

--- showsz.cpp
+++ showsz.cpp
@@ -1,4 +1,7 @@
 #include "densityxx/api.hpp"
+#include "densityxx/chameleon.hpp"
+#include "densityxx/cheetah.hpp"
+#include "densityxx/lion.hpp"
 
 #define SHOWSZ(TYPE) printf("sizeof(%s) = %u\n", #TYPE, (unsigned)sizeof(TYPE))
 int
@@ -8,5 +11,23 @@ main(void)
     SHOWSZ(density::stream_header_information_t);
     SHOWSZ(density::encode_t);
     SHOWSZ(density::decode_t);
+    SHOWSZ(density::main_header_t);
+    SHOWSZ(density::main_footer_t);
+    SHOWSZ(density::block_encode_t);
+    SHOWSZ(density::block_decode_t);
+    SHOWSZ(density::block_header_t);
+    SHOWSZ(density::block_mode_marker_t);
+    SHOWSZ(density::block_footer_t);
+    SHOWSZ(density::location_t);
+    SHOWSZ(density::teleport_t);
+    SHOWSZ(density::chameleon_dictionary_t);
+    SHOWSZ(density::chameleon_encode_t);
+    SHOWSZ(density::chameleon_decode_t);
+    SHOWSZ(density::cheetah_dictionary_t);
+    SHOWSZ(density::cheetah_encode_t);
+    SHOWSZ(density::cheetah_decode_t);
+    SHOWSZ(density::lion_dictionary_t);
+    SHOWSZ(density::lion_encode_t);
+    SHOWSZ(density::lion_decode_t);
     return 0;
 }
